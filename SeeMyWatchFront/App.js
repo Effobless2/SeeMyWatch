78ed875bb82e916910c0aa7bfaca931214b86a77
cylia boukhiba <cylia.silouh@gmail.com>
2019-10-16 20:46:21 +0200

    modif main

--- SeeMyWatchFront/App.js
+++ SeeMyWatchFront/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View , StatusBar} from 'react-native';
-import Form from "./Components/Form"
-import ViewLogin from "./Components/ViewLogin"
-import FormActivity from "./Components/FormActivity"
+import { View, Button} from 'react-native';
+import Main from './Pages/Main';
+import FormActivity from './Components/FormActivity';
+//import Cadrans from './Pages/Cadrans';
+
+
+
 export default class App extends React.Component{
-  render(){
+  constructor(props) {
+    super(props);
+    this.state= {
+      Screen: 1
+    }
+  }
+  
+         
+
+
+render() {
+  if (this.state.Screen == 0){
 
     return (
       <View>
-        <FormActivity/>
+        <Main/>
+        <Button title="gotoview1" onPress={() => {this.setState({Screen : 1})}}/>
+
       </View>
-        
-    
-  );
-}
+    )
+  }
+  else{
+    return (
+      <View>
+        <FormActivity/>
+        <Button title="gotoview0" onPress={() => {this.setState({Screen : 0})}}/>
+      </View>
+    )
+  }
 }
 
-/*const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});*/
+}
+
+
+
+
+
+
+
+
+
+
+  

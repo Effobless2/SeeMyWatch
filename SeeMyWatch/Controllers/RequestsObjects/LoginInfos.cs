eb2fa4e57b4f2a6e8bab7d51a480102153c3bf24
Maxime Deboffle <maxime.deboffle@hotmail.com>
2019-10-16 20:02:13 +0200

    Re ancien commit

---
+++ SeeMyWatch/Controllers/RequestsObjects/LoginInfos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeeMyWatch.Controllers.RequestsObjects
+{
+    public class LoginInfos
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ SeeMyWatchFront/store/passerrelle.js
@@ -0,0 +1,13 @@
+export default class Passerelle {
+    async Connection(login, password){
+        let res = await fetch(`http://192.168.43.220:44338/Account/Login`,{
+            method:"POST",
+            body: JSON.stringify({login: login, password : password}),
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            }
+        }).catch((result) => {return result;})
+        return res.status;
+    }
+}
